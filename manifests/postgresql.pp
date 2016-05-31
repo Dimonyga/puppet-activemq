e64aeabbddc33bb0194f3f27d293b21853c4b2dc
Dimonyga <Dimonyga@gmail.com>
2016-05-31 15:17:10 +0600

    add postgresql properties

--- manifests/postgresql.pp
+++ manifests/postgresql.pp
@@ -8,12 +8,25 @@ class activemq::postgresql (
   $database_name        = 'jmx',
   $database_username    = 'jmx',
   $database_password    = 'jmx',
-) {
+) inherits activemq {
 
   postgresql::server::db { $database_name:
     user     => $database_username,
     password => $database_password,
     grant    => 'all',
+  }
+
+  $str = "## activemq database settings
+          db.host=localhost
+          db.port=5432
+          db.name=${database_name}
+          db.user=a${database_username}
+          db.passwd=${database_password}
+          db.pool.maxActive=10
+          "
+
+  file { "${real_config_dir}/activemq.properties":
+    content => $str,
   }
 
 }
